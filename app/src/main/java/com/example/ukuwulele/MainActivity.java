1d4746005ebfa59818d2dc5e8d165a3ecb4e2542
tomasswaier <tomasswaier@gmail.com>
2024-07-28 09:42:23 +0200

    alpha

--- app/src/main/java/com/example/ukuwulele/MainActivity.java
+++ app/src/main/java/com/example/ukuwulele/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.ukuwulele;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.os.Build.VERSION.SDK_INT;
 
-import android.content.Context;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,100 +10,154 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import java.io.File;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.Manifest;
-import android.os.Environment;
-import android.provider.Settings;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioGroup;
-import android.widget.RadioButton;
 
+import java.io.File;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int REQUEST_CODE = 123; // You can use any integer value here
-    private static final int PERMISSION_REQUEST_CODE =321;
+    private static final int REQUEST_CODE = 123;
+    private static final int PERMISSION_REQUEST_CODE = 321;
     private RadioGroup rgGroup;
     String[] filenames;
+    String choseFile;//imsorry
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         rgGroup = findViewById(R.id.rgGroup);
-        String[] downloadsContents;
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        dirList();
+
         Button btnPlay = findViewById(R.id.BtnPlay);
         btnPlay.setOnClickListener(this);
 
-
+        // Request necessary permissions
+        requestNecessaryPermissions();
     }
-    public void dirList(){
+
+
+    public void dirList() {
         if (SDK_INT >= Build.VERSION_CODES.R) {
-            if (Environment.isExternalStorageManager()) {
-                startActivity(new Intent(this, MainActivity.class));
-            } else { //request for the permission
+            if (!Environment.isExternalStorageManager()) {
+                System.out.println("wat PROBLEM");
+                requestNecessaryPermissions();
+                return;
+            }
+        } else {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                System.out.println("wat PROBLEM");
+                requestNecessaryPermissions();
+                return;
+            }
+        }
+
+        File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        if (downloadsFolder.exists() && downloadsFolder.isDirectory()) {
+            File audiobooksFolder = new File(downloadsFolder, "audiobooks");
+            if (audiobooksFolder.exists() && audiobooksFolder.isDirectory()) {
+                File[] audiobookFiles = audiobooksFolder.listFiles();
+                if (audiobookFiles != null) {
+                    filenames = new String[audiobookFiles.length];
+                    for (int i = 0; i < audiobookFiles.length; i++) {
+                        filenames[i] = audiobookFiles[i].getName();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, play.class);
+        intent.putExtra("SONG_NAME",choseFile);
+        startActivity(intent);
+    }
+
+    public void display_folder_insides(View view) {
+        dirList();
+        System.out.println(Arrays.toString(filenames));
+        for(String filename: filenames) {
+            System.out.println(filename);
+            addButton(filename);
+
+        }
+    }
+
+    public void addButton(String name){
+        LinearLayout layout = (LinearLayout) findViewById(R.id.main);
+        Button newbtn= new Button(this);
+        newbtn.setText(name);
+        newbtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                choseFile=name;
+                System.out.println(choseFile);
+
+            }
+        });
+        layout.addView(newbtn);
+    }
+
+    private void requestNecessaryPermissions() {
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+
                 Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
                 Uri uri = Uri.fromParts("package", getPackageName(), null);
                 intent.setData(uri);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         } else {
-            //below android 11=======
-            startActivity(new Intent(this, MainActivity.class));
-            ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    WRITE_EXTERNAL_STORAGE
+            }, PERMISSION_REQUEST_CODE);
         }
-        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            // Permission is granted, so you can access the Downloads folder
-            File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            if (downloadsFolder.exists() && downloadsFolder.isDirectory()) {
-                // Downloads folder exists and is a directory
-                File audiobooksFolder = new File(downloadsFolder, "audiobooks");
-                if (audiobooksFolder.exists() && audiobooksFolder.isDirectory()) {
-                    // audiobooks folder exists and is a directory
-                    // Now you can access the files inside the audiobooks folder
-                    File[] audiobookFiles = audiobooksFolder.listFiles();
-                    if (audiobookFiles != null) {
-                        filenames=new String[audiobookFiles.length];
-                        for(int i=0;i<audiobookFiles.length;i++){
-                            filenames[i]=audiobookFiles[i].getName();
-                            System.out.println(filenames[i]);
-                        }
-
-                    }
-                    // Loop through audiobookFiles array to access individual files
-                } else {
-                    // audiobooks folder does not exist or is not a directory
-                    // Handle this case
-                }
-            } else {
-                // Downloads folder does not exist or is not a directory
-                // Handle this case
-            }
-        } else {
-            // Permission is not granted, so request it
-            System.out.println("PROBLEM");
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);        }
-
-
     }
-    @Override
-    public void onClick(View v) {
-        int checkedId=rgGroup.getCheckedRadioButtonId();
-        System.out.println(checkedId);
-        Intent intent=new Intent(this,play.class);
-        startActivity(intent);
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+                    dirList();
+                } else {
+                    System.out.println("Permission denied");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                dirList();
+            } else {
+                // Permission is denied
+                System.out.println("Permission denied");
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ukuwulele/play.java
+++ app/src/main/java/com/example/ukuwulele/play.java
@@ -1,9 +1,13 @@
 package com.example.ukuwulele;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+
 public class play extends AppCompatActivity implements View.OnClickListener {
     Button button;
     MediaPlayer player;
     boolean playing;
-
+    String filename;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,10 @@ public class play extends AppCompatActivity implements View.OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent intent = getIntent();
+        filename=intent.getStringExtra("SONG_NAME");
+        textView = (TextView) findViewById(R.id.txtName);
+        textView.setText(filename);
         playing = false;
         button = findViewById(R.id.button);
 
@@ -36,7 +47,10 @@ public class play extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v) {
         if (!playing) {
             if (player == null) {
-                player = MediaPlayer.create(play.this, R.raw.ic_examplesong);
+                File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File audiobooksFolder = new File(downloadsFolder, "audiobooks");
+                player = MediaPlayer.create(play.this, Uri.fromFile(new File(audiobooksFolder,filename)));
+                System.out.println("test");
             }
             player.start();
             playing=true;
